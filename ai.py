7270d2ce2de15df74cf1a69a968ac1842a3c318a
Doychin Atanasov <ironsmile@gmail.com>
2018-02-03 23:40:01 +0200

    Refactor ai.py: pep8

--- ai.py
+++ ai.py
@@ -4,12 +4,18 @@
 import random
 from locals import *
 
+
 class AI (object):
 
     fire_chance = 1
     moving_chance = 70
     turning_chance = 5
-    turns_list = [DIRECTION_UP, DIRECTION_DOWN, DIRECTION_LEFT, DIRECTION_RIGHT]
+    turns_list = [
+        DIRECTION_UP,
+        DIRECTION_DOWN,
+        DIRECTION_LEFT,
+        DIRECTION_RIGHT,
+    ]
 
     def tick(self, enemies):
 
@@ -20,8 +26,8 @@ class AI (object):
                 enemy.fire()
 
             turning_roll = random.randint(0, 100)
-            if  turning_roll <= self.turning_chance:
-                turn_index = random.randint(0,3)
+            if turning_roll <= self.turning_chance:
+                turn_index = random.randint(0, 3)
                 turn_direction = self.turns_list[turn_index]
                 enemy.move(turn_direction)
                 continue
